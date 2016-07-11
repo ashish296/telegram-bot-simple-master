75627c716be422a90a07b0c21bfa99a32fcba818
System Administrator <ashish.verma@nidarooms.com>
2016-07-11 17:40:45 +0530

    Register Telegram  Webhook

---
+++ bot.js
@@ -0,0 +1,24 @@
+var token = '229884956:AAEGSuK8l9Chqcl8uMjty5lz4RnRjaKBPIU';
+var Bot = require('node-telegram-bot-api'),
+    bot = new Bot(token, { polling: true });
+
+console.log('bot server started...');
+
+// hello command
+bot.onText(/^\/say_hello (.+)$/, function (msg, match) {
+  var name = match[1];
+  bot.sendMessage(msg.chat.id, 'Hello ' + name + '!').then(function () {
+    // reply sent!
+  });
+});
+
+// sum command
+bot.onText(/^\/sum((\s+\d+)+)$/, function (msg, match) {
+  var result = 0;
+  match[1].trim().split(/\s+/).forEach(function (i) {
+    result += (+i || 0);
+  })
+  bot.sendMessage(msg.chat.id, result).then(function () {
+    // reply sent!
+  });
+});
